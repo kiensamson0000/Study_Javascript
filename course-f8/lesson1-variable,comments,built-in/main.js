6eeb3f37c7c9eacf94d321a6bbf582e93b74c5f5
Khuat Huy Kien <kienkh99@gmail.com>
2022-02-23 17:26:44 +0700

    update code

---
+++ course-f8/lesson1-variable,comments,built-in/main.js
@@ -0,0 +1,244 @@
+// lesson3-11
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/** Phan 2: Biến, comments, built-in
+ * 
+ * 
+*/ 
+
+// thong bao 
+// alert('Hello world');
+
+
+///////////////////////////////////////////////////////////////////////////
+/**  Lesson 7: 
+ * 
+ * 
+*/ 
+
+// dau '' use nhieu hon ""
+// var fullName = 'duong trung hieu';
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////
+/**  Lesson 11: Hàm built-in là gì? => hàm cài sẵn, chức năng lập sẵn trong js
+ * - Gioi thieu 1 so ham built-in:
+ *  1. alert  => open hop thoai thong bao
+ *  2. console => console trong "console" devtools
+ *  3. confirm => open hop thoai confirm
+ *  4. prompt => open hop thoai prompt
+ *  5. set timeout 
+ *  6. set interval
+ * 
+*/ 
+
+// var fullName = 'DUong van Trung';
+
+// alert(fullName);  
+
+// console.log(fullName); 
+// warn: canh bao, error: loi
+// console.warn(fullName); 
+// console.error(fullName);
+
+
+// confirm('xac nhan ban du tuoi!');
+
+
+// prompt('Ban ten la gi?');
+
+
+/*
+setTimeout(function() {
+    alert('hello world');
+}, 1000);
+*/
+
+
+/*
+setInterval(function() {
+        console.log('day la thong bao')
+    }, 1000);
+*/
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**  Phan 3:  Toán tử, kiểu dữ liệu
+ * 
+ * 
+*/ 
+
+// var a = '15' - 5;
+// console.log(a);
+// console.log(typeof(a));
+
+// Toán tử số học (Arithmetic)
+// Toán tử gán (Assignment)
+// Toán tử so sánh (Comparison)
+// Toán tử logic (Logical)
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/**  Lesson 14: Toán tử số học
+ * 
+ * 
+*/ 
+// Cho biến sum là tổng của a và b, different là hiệu của a và b. Hãy tìm a và b.
+
+/*
+function run(sum, different) {
+    var a = (sum + different) / 2;
+    var b = (sum - different) / 2;
+        
+    return 'a is ' + a + ' and b is ' + b;
+}
+*/
+
+// var a = 5 + '5';
+// console.log(a);
+// console.log(typeof(a));
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/**  Lesson 15: Prefix &  Postfix 
+ * 
+ * 
+*/ 
+
+/*
+var number = 6;
+var output = number++ + --number;
+    //        = 6        +    6
+    // number++ => +1 cho number => number = 6 + 1 = 7 nhung => return = 6
+    // --number => -1 cho number => number = 7 -1 = 6 => return = 6
+
+console.log('output:', output);
+*/
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/**  Lesson 17:  
+ * 
+ * 
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/**  Lesson : 
+ * 
+ * 
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/**  Lesson : 
+ * 
+ * 
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**  Lesson : 
+ * 
+ * 
+*/ 
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**  Lesson : 
+ * 
+ * 
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**  Lesson : 
+ * 
+ * 
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**  Lesson : 
+ * 
+ * 
+*/ 

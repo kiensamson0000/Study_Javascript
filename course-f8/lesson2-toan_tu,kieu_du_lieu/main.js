f77aec87f7bce8120035f4d9d66b3d57348e5315
Khuat Huy Kien <kienkh99@gmail.com>
2022-02-24 22:11:54 +0700

    update code

---
+++ course-f8/lesson2-toan_tu,kieu_du_lieu/main.js
@@ -0,0 +1,55 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**  Phan 3:  Toán tử, kiểu dữ liệu
+ * 
+ * 
+*/ 
+
+// var a = '15' - 5;
+// console.log(a);
+// console.log(typeof(a));
+
+// Toán tử số học (Arithmetic)
+// Toán tử gán (Assignment)
+// Toán tử so sánh (Comparison)
+// Toán tử logic (Logical)
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/**  Lesson 14: Toán tử số học
+ * 
+ * 
+*/ 
+// Cho biến sum là tổng của a và b, different là hiệu của a và b. Hãy tìm a và b.
+
+/*
+function run(sum, different) {
+    var a = (sum + different) / 2;
+    var b = (sum - different) / 2;
+        
+    return 'a is ' + a + ' and b is ' + b;
+}
+*/
+
+// var a = 5 + '5';
+// console.log(a);
+// console.log(typeof(a));
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/**  Lesson 15: Prefix &  Postfix 
+ * 
+ * 
+*/ 
+
+/*
+var number = 6;
+var output = number++ + --number;
+    //        = 6        +    6
+    // number++ => +1 cho number => number = 6 + 1 = 7 nhung => return = 6
+    // --number => -1 cho number => number = 7 -1 = 6 => return = 6
+
+console.log('output:', output);
+*/
---
+++ course-f8/lesson3-xu_ly_chuoi/main.js
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**  Phan 4: Làm việc với chuỗi
+ * 
+ * 
+*/ 
+
+
+// var fullName = 'Duong Van A';
+
+// var fullName = new String('Duong Van A');
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/**  Lesson 27: 
+ *   
+*/   
+
+// var title = 'Học Javascript tại F8';
+
+// function run(title) {
+//     var language = title.slice(4,14);
+//     var rest = title.slice(15, title.length);
+//     return `Language is ${language} and rest is ${rest}`;
+// }
+
+// console.log(run(title));
+
+
+
+
+
+
